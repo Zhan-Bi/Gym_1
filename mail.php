1b0a481fc7ab12255646e305760949302015487b
Zhan-Bi <91236602+Zhan-Bi@users.noreply.github.com>
2022-07-05 17:17:23 +0600

    Update mail.php
    trying to remake 6-yr old php code to newest

--- mail.php
+++ mail.php
@@ -11,29 +11,32 @@ $phone = $_POST['U_phone'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'nekozhanyymeshok@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'LDsa*9$02Ida&'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Host = "smtp.mail.ru";  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               
+
+$mail->Username = "nekozhanyymeshok@mail.ru"; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = "LDsa*9$02Ida&"; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('nekozhanyymeshok@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('kar.one.to.one@gmail.com');     // Кому будет уходить письмо
+$mail->setFrom("nekozhanyymeshok@mail.ru"); // от кого будет уходить письмо?
+$mail->addAddress("kar.one.to.one@gmail.com", "Kar[1]");     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+//$mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Форма с сайта Gym';
 $mail->Body    = '' .$name . ' оставил заявку. Данные:<br> Телефон: ' .$phone. ' <br> Почта: ' .$email;
 $mail->AltBody = '';
 
-if(!$mail->send()) {
-    echo 'Error';
+if(!$mail->Send()) {
+    echo "Error";
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    exit;
 } else {
     header('Location: After_form.html');
 }
